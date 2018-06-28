b70152cf8e3c56d2ce658dafee8a1f36d227eadf
Orange <904412343@qq.com>
2018-06-28 23:04:23 +0800

    Initial commit

---
+++ lib/log_analyzer.ex
@@ -0,0 +1,18 @@
+defmodule LogAnalyzer do
+  @moduledoc """
+  Documentation for LogAnalyzer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LogAnalyzer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/log_analyzer_test.exs
@@ -0,0 +1,8 @@
+defmodule LogAnalyzerTest do
+  use ExUnit.Case
+  doctest LogAnalyzer
+
+  test "greets the world" do
+    assert LogAnalyzer.hello() == :world
+  end
+end
